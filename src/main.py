548f7d83f18486d86323288466028c318d399880
Dmitry <dima.captan@yandex.ru>
2023-05-05 21:46:39 +0300

    add main.py

--- src/main.py
+++ src/main.py
@@ -4,9 +4,12 @@ FILE_URL = "../json_files/operations.json"
 
 
 def main():
-    operations_list = load_file(FILE_URL)
-    operations = make_operations(operations_list)
-    print(get_executed_five(operations))
+    try:
+        operations_list = load_file(FILE_URL)
+        operations = make_operations(operations_list)
+        print(get_executed_five(operations))
+    except FileNotFoundError:
+        print("Файл не найден")
 
 
 if __name__ == "__main__":
